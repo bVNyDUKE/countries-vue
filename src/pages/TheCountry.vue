82c79fe4fdae0b09e24f1619cbd80331bd0ced22
Darko Ivkov <bVNyDUKE@protonmail.com>
2022-11-05 18:15:34 +0100

    wip

--- src/pages/TheCountry.vue
+++ src/pages/TheCountry.vue
@@ -1,43 +1,22 @@
 <script setup>
-import { computed, ref, watchEffect } from 'vue'
-import { useRoute, useRouter } from 'vue-router'
-import { getCountryFromStore, getCountryFromApi } from '../store'
-import CountryLoader from '../components/CountryLoader.vue'
+import { useRoute, useRouter } from "vue-router";
+import { getCountryByCode } from "../api";
+import CountryLoader from "../components/CountryLoader.vue";
 
-const router = useRouter()
-const route = useRoute()
-const country = ref(null)
-const error = ref(null)
-const borders = ref(null)
-const loading = ref(false)
+const router = useRouter();
+const route = useRoute();
 
-watchEffect(async () => {
-  if(route.params.country !== undefined){
-    loading.value = true
-    const { data, error: err } = await getCountryFromApi(route.params.country)
-    country.value = data
-    error.value = err
-
-    if(country.value.borders){
-
-      borders.value = await Promise.all(country.value.borders
-        .map( async code => {
-          const country = await getCountryFromStore(code)
-          return { name: country.name.common, code: country.cca3 }
-        }))
-    }
-    loading.value = false
-  }
-})
-
-const currencies = computed(() => country?.value?.currencies?.reduce((prev, next) => [...prev, next.name], []).join(', '))
-const languages = computed(() => country?.value?.languages?.reduce((prev, next) => [...prev, next.name], []).join(', '))
-const domain = computed(() => country?.value?.topLevelDomain && country.value.topLevelDomain[0])
-
+const {
+  isLoading,
+  isError,
+  data: country,
+} = getCountryByCode(route.params.country);
 </script>
 
 <template>
-  <div class="container mx-auto mt-5 sm:mt-10 p-10 text-blue-955 dark:text-white">
+  <div
+    class="container mx-auto mt-5 sm:mt-10 p-10 text-blue-955 dark:text-white"
+  >
     <div class="mb-20">
       <button
         class="flex justify-center items-center bg-white text-blue-955 dark:bg-blue-950 dark:text-gray-400 w-32 p-2 space-x-2 rounded-sm shadow-2xl"
@@ -57,9 +36,7 @@ const domain = computed(() => country?.value?.topLevelDomain && country.value.to
             d="M7 16l-4-4m0 0l4-4m-4 4h18"
           />
         </svg>
-        <div class="font-semibold">
-          Back
-        </div>
+        <div class="font-semibold">Back</div>
       </button>
     </div>
     <div v-if="loading">
@@ -72,7 +49,7 @@ const domain = computed(() => country?.value?.topLevelDomain && country.value.to
       <img
         class="object-cover sm:w-[645px] sm:h-[465px] float-left"
         :src="country?.flag"
-      >
+      />
 
       <div class="clear-left">
         <div class="space-y-10">
@@ -101,7 +78,7 @@ const domain = computed(() => country?.value?.topLevelDomain && country.value.to
               </p>
               <p>
                 <span class="font-semibold">Capital:</span>
-                {{ country?.capital || 'None' }}
+                {{ country?.capital || "None" }}
               </p>
             </div>
             <div>
@@ -120,19 +97,25 @@ const domain = computed(() => country?.value?.topLevelDomain && country.value.to
             </div>
           </div>
           <div class="w-62 space-y-5">
-            <span class="inline-block text-xl font-bold mr-5">Border Countries:</span>
+            <span class="inline-block text-xl font-bold mr-5"
+              >Border Countries:</span
+            >
             <span
               v-for="border in borders"
               :key="border"
               class="inline-block place-content-center bg-white text-blue-955 dark:bg-blue-950 dark:text-gray-400 px-8 p-2 mx-2 rounded-sm shadow-2xl hover:cursor-pointer"
-              @click="router.push({name: 'Country', params: {country:border.code}})"
-            >{{ border.name }}</span>
+              @click="
+                router.push({
+                  name: 'Country',
+                  params: { country: border.code },
+                })
+              "
+              >{{ border.name }}</span
+            >
           </div>
         </div>
       </div>
     </div>
-    <div v-else>
-      An error occured
-    </div>
+    <div v-else>An error occured</div>
   </div>
 </template>
